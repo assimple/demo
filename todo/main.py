2a84321d1a89eb4b9fd302d13d14815ec244ef93
dell <zhangjing@ubuntu.ubuntu-domain>
2018-05-29 21:12:15 +0800

    render index.html

--- todo/main.py
+++ todo/main.py
@@ -5,7 +5,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return render_template("index.html")
+    todo=[
+        "测试1",
+        "测试2",
+        "测试3",
+        "测试4",
+        "测试5"
+    ]
+    return render_template("index.html", data=todo)
 
 
 if __name__=="__main__":
