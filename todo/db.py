005f606dfae99ab0f2bc0dc94be70d73de5b3f81
dell <zhangjing@ubuntu.ubuntu-domain>
2018-05-31 20:35:08 +0800

    test and finished DELETE api

--- todo/db.py
+++ todo/db.py
@@ -2,29 +2,45 @@ import sqlite3
 
 
 class TodoDB():
+    def __init__(self):
+        self.conn = sqlite3.connect("myDB.db")
+
+    def cursor(self):
+        return self.conn.cursor()
+
+    def close(self):
+        return self.conn.close()
+
+    def commit(self):
+        return self.conn.commit()
+
     def read_all(self):
-        conn = sqlite3.connect("myDB.db")
-        cursor = conn.cursor()
+        cursor = self.cursor()
         cursor = cursor.execute("select id,content from list")
         data = cursor.fetchall()
         cursor.close()
-        conn.commit()
-        conn.close()
         # data = [d[0] for d in data]
-        print(data)
+        # print(data)
         return data
 
     def init_db(self):
         conn = sqlite3.connect('myDB.db')
         cursor = conn.cursor()
-        cursor.execute("create table list(id INTEGER PRIMARY KEY AUTOINCREMENT,content varchar(500),time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,title varchar(100))")
-        cursor.execute("create table user (id INTEGER PRIMARY KEY AUTOINCREMENT, username varchar(20), userpwd varchar(30))")
+        cursor.execute(
+            "create table list(id INTEGER PRIMARY KEY AUTOINCREMENT,content varchar(500),time timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP,title varchar(100))")
+        cursor.execute(
+            "create table user (id INTEGER PRIMARY KEY AUTOINCREMENT, username varchar(20), userpwd varchar(30))")
         cursor.close()
         conn.commit()
         conn.close()
 
-    def delete_list(self,list_id):
-        print("delete；",list_id)
+    def delete_list(self, list_id):
+        # 创立链接   获取游标
+        cursor = self.cursor()
+        cursor = cursor.execute("delete from list where id=?", (list_id,))
+        cursor.close()
+        self.commit()
+        return
 
 
 if __name__ == "__main__":
--- todo/main.py
+++ todo/main.py
@@ -6,15 +6,18 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def hello():
+def index():
     db = TodoDB()
     todo = db.read_all()
+    db.close()
     return render_template("index.html", data=todo)
+
 
 @app.route('/todo/<int:list_id>',methods=["DELETE"])
 def delete(list_id):
     db = TodoDB()
     todo = db.delete_list(list_id)
+    db.close()
     return "okey"
 
 
